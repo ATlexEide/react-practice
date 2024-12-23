edfb9127b07680a56809d9bdefcd2b7dfe00677e
Alex <atlexeide@gmail.com>
2024-12-24 00:00:30 +0100

    Add header

---
+++ src/Greeting.jsx
@@ -0,0 +1,11 @@
+function Greeting() {
+  return <p>This is a very nice JSX greeting</p>;
+}
+export function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
+export default Greeting;
