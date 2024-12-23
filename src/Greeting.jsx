da8cbc8f035150d782abb15e3a758e696e0d393d
Alex <atlexeide@gmail.com>
2024-12-24 00:12:12 +0100

    Update exports

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,11 +1,11 @@
 function Greeting() {
   return <p>This is a very nice JSX greeting</p>;
 }
-export function Header() {
+function Header() {
   return (
     <header>
       <h1>Header</h1>
     </header>
   );
 }
-export default Greeting;
+export { Greeting as JSXGreeting, Header as MainHeader };
