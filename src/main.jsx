e83376e2bad15eddba68c72b392708be82139e57
Alex <atlexeide@gmail.com>
2024-12-24 01:09:26 +0100

    Render test component

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { JSXGreeting, MainHeader } from "./Greeting.jsx";
+import { Test } from "./Test.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MainHeader />
     <JSXGreeting />
+    <Test />
   </StrictMode>
 );
