ef0847edfb63f7bea1694231ceb84521d5027d16
Alex <atlexeide@gmail.com>
2024-12-25 19:33:00 +0100

    .

--- src/Test.jsx
+++ src/Test.jsx
@@ -13,3 +13,7 @@ export function List(props) {
 export function FullList(props) {
   return <List animals={props.animals} />;
 }
+const bool = false;
+export function PrintBool() {
+  if (bool) return <h1>Is true</h1>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { JSXGreeting, MainHeader } from "./Greeting.jsx";
-import { List } from "./Test.jsx";
+import { List, PrintBool } from "./Test.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MainHeader />
     <JSXGreeting />
     <List animals={["Dog", "Cat", "Bird"]} />
+    <PrintBool />
   </StrictMode>
 );
