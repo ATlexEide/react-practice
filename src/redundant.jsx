495d2b8f6c3e3c4b597f1647042fe8b71f0ff628
Alex <atlexeide@gmail.com>
2025-01-19 15:55:05 +0100

    Add redundant stste practice

---
+++ src/redundant.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Let’s check you in</h2>
+      <label>
+        First name: <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name: <input value={lastName} onChange={handleLastNameChange} />
+      </label>
+      <p>
+        Your ticket will be issued to: <b>{`${firstName} ${lastName}`}</b>
+      </p>
+    </>
+  );
+}
